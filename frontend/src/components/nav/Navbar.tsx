75764e9a2d5c5d9edddf0662b5d44010c9efd667
heydoyouknowme0 <akrb2204@gmail.com>
2023-12-27 02:49:58 +0530

    cookie cutter

--- frontend/src/components/nav/Navbar.tsx
+++ frontend/src/components/nav/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
 
   const handleLogout = async () => {
     try {
-      await axios.get(`/api/auth/logout`);
+      await axios.get(`/api/auth/logout`, { withCredentials: true });
       setUser(null);
       await verifyAuth();
       toast.success("Logged out successfully");
